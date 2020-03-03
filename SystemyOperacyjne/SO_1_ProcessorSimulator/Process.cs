cf2f5e465c753d097a18a25277a9f9db282da1d4
JanWielgus <jan.wielgus12@gmail.com>
2020-03-03 22:17:29 +0100

    Some initial development of task 1 on SO
    Systemy Operacyjne zadanie 1

---
+++ SystemyOperacyjne/SO_1_ProcessorSimulator/Process.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_1_ProcessorSimulator
+{
+    class Process
+    {
+        private int totalExecutionTime = 0;
+        private int remainingExecutionTime = 0;
+        private bool isDone = false;
+        private int id;
+        private static int lastGivenID = 0;
+
+
+
+        public Process(int executionTime)
+        {
+            // Give an ID to this task
+            lastGivenID++;
+            id = lastGivenID;
+
+            TotalExecutionTime = executionTime;
+            RemainingExecutionTime = TotalExecutionTime;
+        }
+
+
+        // Final taks execution
+        // Just shows a message
+        private void execute()
+        {
+            Console.WriteLine("Task " + id + " has just ended!");
+            isDone = true;
+        }
+
+
+
+
+        
+        // >>>>>>>>>>> Properties
+
+
+        public int TotalExecutionTime
+        {
+            get { return totalExecutionTime; }
+            private set
+            {
+                if (value > 0)
+                    totalExecutionTime = value;
+                else
+                    totalExecutionTime = 0;
+            }
+        }
+        public int RemainingExecutionTime
+        {
+            get { return remainingExecutionTime; }
+            private set
+            {
+                if (value > 0)
+                    remainingExecutionTime = value;
+                else
+                    remainingExecutionTime = 0;
+            }
+        }
+
+        public bool IsDone { get { return isDone; } }
+
+
+
+
+        // >>>>>>>>>>> Methods
+
+
+        // Executes this process for a given time
+        // May be not ended in a given time
+        // Autimatically executes execute() method when done
+        public void executeFor(int time)
+        {
+            RemainingExecutionTime -= time;
+
+            // If the whole task was completed then show the result
+            if (RemainingExecutionTime == 0)
+                execute();
+            else
+                Console.WriteLine("Task " + id + " suspended and waiting (remain " +
+                    RemainingExecutionTime + " of total " + TotalExecutionTime + ")");
+        }
+    }
+}
---
+++ SystemyOperacyjne/SO_1_ProcessorSimulator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SO_1_ProcessorSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Process testProcess = new Process(10);
+
+            testProcess.executeFor(4);
+            testProcess.executeFor(4);
+            testProcess.executeFor(4);
+
+        }
+    }
+}
