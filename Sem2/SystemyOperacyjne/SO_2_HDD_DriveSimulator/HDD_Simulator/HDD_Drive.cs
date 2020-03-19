41a063ba43604a74869745c2461e282d292ed01c
JanWielgus <jan.wielgus12@gmail.com>
2020-03-19 17:49:51 +0100

    Create HDD_Drive.cs

---
+++ Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/HDD_Simulator/HDD_Drive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*
+ * HDD Drive have an arm and have sum of the all movements
+ * 
+ * */
+
+
+namespace SO_2_HDD_DriveSimulator.HDD_Simulator
+{
+    class HDD_Drive
+    {
+        private Algorithms.SchedulingAlgorithm schedulingAlgorithm;
+        private Sum armMovementSum;
+
+
+        HDD_Drive()
+        {
+            armMovementSum = new Sum();
+        }
+    }
+}
