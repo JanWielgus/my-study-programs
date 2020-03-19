15ecbb641bbe736a8771087cf9bbc755ab1e2db8
JanWielgus <jan.wielgus12@gmail.com>
2020-03-19 16:47:53 +0100

    Update Program.cs

--- Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/Program.cs
+++ Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+
+/*
+ * This simulation is very simple
+ * For example do not include disc arm movement
+ * Time to reach memory address is proportional to the difference between addresses
+ * */
+
 namespace SO_2_HDD_DriveSimulator
 {
     class Program
