30a46aba2d78b942b94d6f952727bb1e77bcd236
JanWielgus <jan.wielgus12@gmail.com>
2020-03-19 16:46:44 +0100

    Created global constants class

---
+++ Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/HDD_Simulator/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_2_HDD_DriveSimulator.HDD_Simulator
+{
+    class Constants
+    {
+        public const int FirstMemoryUnitAddr = 0;
+        public const int LastMemoryUnitAddr = 1000;
+    }
+}
