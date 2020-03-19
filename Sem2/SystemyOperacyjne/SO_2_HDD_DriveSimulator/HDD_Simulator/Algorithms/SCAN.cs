47e6567d988b4e3eb676aba44bc638d71d23f377
JanWielgus <jan.wielgus12@gmail.com>
2020-03-19 22:37:58 +0100

    SCAN is ready for testing

--- Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/HDD_Simulator/Algorithms/SCAN.cs
+++ Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/HDD_Simulator/Algorithms/SCAN.cs
@@ -2,13 +2,70 @@
 using System.Collections.Generic;
 using System.Text;
 
+/*
+ * SCAN - Algorithm works like elevator (elevetor algotirhm)
+ * Drive arm moves in one direction executing instructions on its path.
+ * When reaches the end it begin to scan in the opposite way.
+ * */
+
 namespace SO_2_HDD_DriveSimulator.HDD_Simulator.Algorithms
 {
-    class SCAN : SchedulingAlgorithm
+    class SCAN : ScanBase
     {
         public override Instruction getNextInstruction()
         {
-            throw new NotImplementedException();
+            // If there are any tasks throw an exception
+            if (instructionList.Count == 0)
+                throw new IndexOutOfRangeException("Instruction list is empty");
+
+
+            Instruction closestInstr = getClosestInstruction(movingDirection);
+
+            // If result is null then change the direction and try again
+            if (closestInstr == null)
+            {
+                // Change direction
+                movingDirection = movingDirection == Direction.FORWARD ? Direction.BACKWARD : Direction.FORWARD;
+
+                return getClosestInstruction(movingDirection);
+            }
+            else
+                return closestInstr;
         }
+
+
+        public override void addInstruction(Instruction instruction)
+        {
+            base.addInstruction(instruction);
+
+
+            if (instructionList.Count == 0)
+                instructionList.Add(instruction);
+            else
+            {
+                // Add in the order (avoid sorting)
+
+                // If new address is before the first instruction already in the list
+                if (instruction.getAddress() < instructionList[0].getAddress())
+                    instructionList.Insert(0, instruction);
+
+                // Find the proper place
+                else
+                {
+                    for (int i=1; i<instructionList.Count; i++)
+                    {
+                        // If new address is between previous and next and break
+                        if (instruction.getAddress() >= instructionList[i-1].getAddress() &&
+                            instruction.getAddress() <= instructionList[i].getAddress())
+                        {
+                            instructionList.Insert(i, instruction);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
---
+++ Sem2/SystemyOperacyjne/SO_2_HDD_DriveSimulator/HDD_Simulator/Algorithms/ScanBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_2_HDD_DriveSimulator.HDD_Simulator.Algorithms
+{
+    abstract class ScanBase : SchedulingAlgorithm
+    {
+        protected enum Direction
+        {
+            FORWARD,
+            BACKWARD
+        }
+
+        protected Direction movingDirection = Direction.FORWARD;
+
+
+        protected Instruction getClosestInstruction(Direction direction)
+        {
+            foreach (Instruction instr in instructionList)
+            {
+                if (direction == Direction.FORWARD)
+                {
+                    if (instr.getAddress() >= driveArm.getCurrentAddress())
+                        return instr;
+                }
+                else if (direction == Direction.BACKWARD)
+                {
+                    if (instr.getAddress() <= driveArm.getCurrentAddress())
+                        return instr;
+                }
+            }
+
+            // if nothing was found - reached end of a memory address
+            return null;
+        }
+    }
+
+}
