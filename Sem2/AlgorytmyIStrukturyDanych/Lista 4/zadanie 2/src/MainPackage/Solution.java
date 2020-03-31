66ea83c5d2f240f693b5d2f9f3980a5d85fffcbd
JanWielgus <jan.wielgus12@gmail.com>
2020-03-31 15:32:19 +0200

    Misunderstood the task. Upload the solution

--- Sem2/AlgorytmyIStrukturyDanych/Lista 4/zadanie 2/src/MainPackage/Solution.java
+++ Sem2/AlgorytmyIStrukturyDanych/Lista 4/zadanie 2/src/MainPackage/Solution.java
@@ -1,64 +1,30 @@
 package MainPackage;
 
-import java.util.ArrayList;
+/*
+
+https://app.codility.com/programmers/lessons/16-greedy_algorithms/tie_ropes/
+
+ */
 
 public class Solution
 {
     public int solution(int K, int[] A)
     {
-        // Put everything inside the ArrayList to enable removing elements
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int temp: A)
-            arr.add(temp);
+        int maxAmtOfRopes = 0; // result
+        int currentLength = 0; // current length of tied ropes (reset when greater than K)
 
-        int maxAmtOfRopes = 0;
-
-        while(findAndRemoveNextRopes(K, arr))
+        for (int i=0; i<A.length; i++)
         {
-            maxAmtOfRopes++;
-        }
+            currentLength += A[i];
 
-        return maxAmtOfRopes;
-    }
-
-    private boolean findAndRemoveNextRopes(int K, ArrayList<Integer> arr)
-    {
-        int i=0;
-        int currentRopeLength = 0;
-
-        // Tie the minimum amt of ropes to be higher than K
-        while (currentRopeLength <= K)
-        {
-            // Return false if rope is not long enough
-            // But there are no ropes available
-            if (arr.size() == 0)
-                return false;
-
-            // Tie down the longest rope available
-            int curID = indexOfTheBiggestValue(arr);
-            currentRopeLength += arr.get(curID);
-            // And remove it from the available ropes array
-            arr.remove(curID);
-        }
-
-        return true;
-    }
-
-    private int indexOfTheBiggestValue(ArrayList<Integer> A)
-    {
-        int biggest = A.get(0);
-        int biggestID = 0;
-
-        for (int i=1; i<A.size(); i++)
-        {
-            if (A.get(0) > biggest)
+            if (currentLength >= K)
             {
-                biggest = A.get(0);
-                biggestID = i;
+                maxAmtOfRopes++;
+                currentLength = 0;
             }
         }
 
-        return biggestID;
+        return maxAmtOfRopes;
     }
 
 }
