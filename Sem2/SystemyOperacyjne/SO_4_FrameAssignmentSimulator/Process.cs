8ddc8297841931e58d2689d8ca545e7da211ea83
JanWielgus <jan.wielgus12@gmail.com>
2020-05-12 22:18:42 +0200

    Process and VirtualMemory class finished

---
+++ Sem2/SystemyOperacyjne/SO_4_FrameAssignmentSimulator/Process.cs
@@ -0,0 +1,99 @@
+﻿using SO_4_FrameAssignmentSimulator.DataContainers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_4_FrameAssignmentSimulator
+{
+	class Process
+	{
+		private readonly int ProcessSize;
+		private int remainingMemoryAccesses;
+		private List<Page> localPageList = null;
+		private int lastCalledPageIdx;
+		private int localPageFaultCounter;
+		private VirtualMemory virtualMemory;
+		private Random rand;
+
+
+		public Process(int processSize)
+		{
+			ProcessSize = processSize;
+			remainingMemoryAccesses = ProcessSize;
+			localPageFaultCounter = 0;
+
+			rand = new Random();
+		}
+
+		public void setVirtualMemory(VirtualMemory vm)
+		{
+			this.virtualMemory = vm;
+		}
+
+
+		public void setPageList(List<Page> pageList)
+		{
+			this.localPageList = pageList;
+			lastCalledPageIdx = 0;
+		}
+
+		public List<Page> getPageList()
+		{
+			return localPageList;
+		}
+
+
+		public int getRemainingMemoryAccessesCount()
+		{
+			return remainingMemoryAccesses;
+		}
+
+
+		// Return false, if process was fully executed and can be removed
+		public bool execute()
+		{
+			// Something is wrong if calling this process when it has been fully executed
+			if (localPageList.Count == 0)
+				throw new Exception("Cos jest nie tak. To zadanie juz zostalo wykonane");
+
+
+			// Prepare page to access
+			Page pageToAccess;
+			int randNum = rand.Next(0, 1);
+			// 50% chance to use last page again
+			if (randNum == 0)
+				pageToAccess = localPageList[lastCalledPageIdx];
+			else
+			{
+				randNum = rand.Next(0, localPageList.Count - 1);
+				pageToAccess = localPageList[randNum];
+				lastCalledPageIdx = randNum;
+			}
+
+
+			// Simulate memory access
+			bool memResult = virtualMemory.simulateGetDataByPage(pageToAccess);
+			if (memResult == false)
+				localPageFaultCounter++;
+
+			// Decrease remaining memory accesses
+			remainingMemoryAccesses--;
+
+			// If this task was fulle executed
+			if (remainingMemoryAccesses == 0)
+			{
+				// Remove all pages from ram
+				virtualMemory.removePageListFromMemory(localPageList);
+				return false;
+			}
+
+			return true;
+		}
+
+
+		public int getLocalPageFaultCounter()
+		{
+			return localPageFaultCounter;
+		}
+	}
+}
--- Sem2/SystemyOperacyjne/SO_4_FrameAssignmentSimulator/VirtualMemory.cs
+++ Sem2/SystemyOperacyjne/SO_4_FrameAssignmentSimulator/VirtualMemory.cs
@@ -24,14 +24,61 @@ namespace SO_4_FrameAssignmentSimulator
 		}
 
 
-		public void simulateGetDataByPage(Page page)
+		// Return false if page failure occured (increase page counter in process)
+		public bool simulateGetDataByPage(Page page)
 		{
-			throw new NotImplementedException();
-			// TODO: asdfjaskldfj;alsdjf;laskjf;alsjf;alskdfj;alksdfj;aslkdfjas;ldkfja;slkfjdf   <<<<<<<
+			bool returnValue = true;
 
+			// Add page to the memory if it already is not
+			if (!page.isInRam())
+			{
+				// If memory is full use LRU to remove one frame from memory
+				if (ram.isFull())
+				{
+					// Increase counters
+					pageFaultCounter++;
+					returnValue = false;
+
+					removeFrameFromRAM_LRU();
+				}
+
+				// Add page to the ram
+				ram.addToMemory(page);
+			}
 
 
 			page.increaseReferenceCounter();
+			return returnValue;
+		}
+
+
+		private void removeFrameFromRAM_LRU()
+		{
+			// !!!!!!!!!!!!!!!!!!!!!
+			// 
+			// Do przerobienia
+
+			// Find least recently used page
+			int lruFrameIdx = 0;
+			List<Frame> ramFrameList = ram.getFrameList();
+			for (int i = 1; i < ramFrameList.Count; i++)
+			{
+				if (ramFrameList[i].getReferenceCounter() > ramFrameList[lruFrameIdx].getReferenceCounter())
+					lruFrameIdx = i;
+			}
+
+			ram.removeFrameByID(ramFrameList[lruFrameIdx].getID());
+		}
+
+
+		// Used by process if it is done
+		public void removePageListFromMemory(List<Page> pageListToRemove)
+		{
+			foreach (Page pg in pageListToRemove)
+			{
+				if (pg.isInRam())
+					ram.removeFrameByID(pg.getID());
+			}
 		}
 	}
 }
