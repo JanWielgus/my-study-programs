92a1776997284c46da3a28f5b9d2727168b36e51
JanWielgus <jan.wielgus12@gmail.com>
2020-06-01 14:35:25 +0200

    Process class is ready

---
+++ Sem2/SystemyOperacyjne/SO_5_DistributedAlgorithmSimulator/Process.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_5_DistributedAlgorithmSimulator
+{
+	class Process
+	{
+		private static int lastID = 0;
+		private readonly int processID;
+		private readonly int processSize;
+		private int processRemainingSize;
+		private float loadOnProcessor;
+
+		
+		public Process(int processSize, float loadOnProcessor)
+		{
+			processID = lastID++;
+
+			this.processSize = processSize;
+			processRemainingSize = processSize;
+
+			this.loadOnProcessor = loadOnProcessor;
+		}
+
+
+		public void compute(int amountComputed)
+		{
+			processRemainingSize -= amountComputed;
+		}
+
+
+		public bool isDone()
+		{
+			return processRemainingSize <= 0;
+		}
+
+
+		// getters
+
+		public int getID()
+		{
+			return processID;
+		}
+
+		public int getSize()
+		{
+			return processSize;
+		}
+
+		public int getRemainingSize()
+		{
+			return processRemainingSize;
+		}
+
+		public float getLoadOnProcessor()
+		{
+			return loadOnProcessor;
+		}
+	}
+}
