c454f51bc3b8836666174a864d684e1c53141596
JanWielgus <jan.wielgus12@gmail.com>
2020-06-01 14:16:17 +0200

    Created project for task 5 solution

---
+++ Sem2/SystemyOperacyjne/SO_5_DistributedAlgorithmSimulator/GlobalClock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_5_DistributedAlgorithmSimulator
+{
+	class GlobalClock
+	{
+		private uint currentTime;
+
+		public uint getCurrentTime()
+		{
+			return currentTime;
+		}
+
+		public void tick()
+		{
+			currentTime++;
+		}
+
+		public void reset()
+		{
+			currentTime = 0;
+		}
+	}
+}
