bc33bbba75645e0c3b4d0c408548537eb69bd2cc
JanWielgus <jan.wielgus12@gmail.com>
2020-06-03 09:25:24 +0200

    Update SimulationConfig.cs

--- Sem2/SystemyOperacyjne/SO_5_DistributedAlgorithmSimulator/SimulationConfig.cs
+++ Sem2/SystemyOperacyjne/SO_5_DistributedAlgorithmSimulator/SimulationConfig.cs
@@ -29,11 +29,11 @@ namespace SO_5_DistributedAlgorithmSimulator
 
 		// my config values
 		public readonly float percentOfTasksToTransfer = 20;    // value between 0 and 100
-		public readonly int averageTimeBetweenNewTask = 3;      // 3 time units average between new task is added
+		public readonly int averageTimeBetweenNewTask = 1;      // 3 time units average between new task is added
 		public readonly int amtOfProcesses = 10000;
 
 		public readonly int minProcessSize = 100;
-		public readonly int maxProcessSize = 5000; // assuming that process sized 100 can be computed in one cycle if use 100% load
+		public readonly int maxProcessSize = 1000; // assuming that process sized 100 can be computed in one cycle if use 100% load
 
 		public readonly float minProcessLoad = 0.5f;
 		public readonly float maxProcessLoad = 8;
