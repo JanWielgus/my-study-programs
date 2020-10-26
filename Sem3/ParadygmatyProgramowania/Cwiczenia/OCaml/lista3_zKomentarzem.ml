9fbe1b55f886fa16f69aabbb666053e5a93449b3
Jan Wielgus <jan.wielgus12@gmail.com>
2020-10-26 10:07:45 +0100

    [paradyg cw] Added exercise 5 list 3 partial solution

--- Sem3/ParadygmatyProgramowania/Cwiczenia/OCaml/lista3_zKomentarzem.ml
+++ Sem3/ParadygmatyProgramowania/Cwiczenia/OCaml/lista3_zKomentarzem.ml
@@ -33,3 +33,37 @@ let sumProd xs =
             List.fold_left (fun x y -> x*y) 1 xs)
     ;;
 
+
+
+
+
+(*Zadanie 5*)
+(*If pred == true then put fst before snd*)
+let insertionSort pred xs =
+    let rec sortedInsert ys a =
+        match ys with
+        | [] -> [a]
+        | head::tail -> if pred a head then a :: ys
+                        else head :: sortedInsert tail a
+        in
+    
+    let rec insertionSortRec toSort sorted =
+        match toSort with
+        | [] -> sorted
+        | head::tail -> insertionSortRec tail (sortedInsert sorted head)
+        in
+
+    match xs with
+    | [] -> []
+    | [elem] -> [elem]
+    | head::tail -> insertionSortRec xs []
+    ;;
+
+insertionSort (fun a b -> a < b) [3; 1; 5; 2; 4] = [1; 2; 3; 4; 5];;
+insertionSort (fun a b -> a > b) [5; 2; -5; 0; 5; 2; -10] = [5; 5; 2; 2; 0; -5; -10];;
+insertionSort (fun a b -> a < b) [] = [];;
+insertionSort (fun a b -> a < b) [3] = [3];;
+
+
+
+
