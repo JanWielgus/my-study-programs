ff06c13b7dd2b5b15f9894155ac9071e70116b19
Jan Wielgus <jan.wielgus12@gmail.com>
2020-10-26 15:12:48 +0100

    Do dokonczenia lista 3 zad 5 paradygmaty

--- Sem3/ParadygmatyProgramowania/Cwiczenia/OCaml/lista3_zKomentarzem.ml
+++ Sem3/ParadygmatyProgramowania/Cwiczenia/OCaml/lista3_zKomentarzem.ml
@@ -37,7 +37,7 @@ let sumProd xs =
 
 
 
-(*Zadanie 5*)
+(*Zadanie 5a*)
 (*If pred == true then put fst before snd*)
 let insertionSort pred xs =
     let rec sortedInsert ys a =
@@ -66,4 +66,54 @@ insertionSort (fun a b -> a < b) [3] = [3];;
 
 
 
+(*Zadanie 5b*)
+let rec mergesort pred xs =
+    let xsLen = List.length xs in
 
+    let rec splitList remain ys half1 half2=
+        match remain with
+        | 0 -> (half1, half2)
+        | _ -> if remain > xsLen/2
+                    then splitList (remain - 1) (List.tl ys) (List.hd ys :: half1) half2
+                else splitList (remain - 1) (List.tl ys) half1 (List.hd ys :: half2)
+        in
+    
+    (*Merge lists and return reversed merged list*)
+    let rec mergeListsRev l1 l2 acc =
+        match (l1, l2) with
+        | ([], []) -> acc
+        | ([], _) -> mergeListsRev l1 (List.tl l2) (List.hd l2 :: acc) (* If l1 is empty, add from l2*)
+        | (_, []) -> mergeListsRev (List.tl l1) l2 (List.hd l1 :: acc) (* If l2 is empty, add from l1*)
+        | (hd1::tl1, hd2::tl2) -> if pred hd1 hd2 then mergeListsRev tl1 tl2 (hd1 :: hd2 :: acc)
+                                    else mergeListsRev tl1 tl2 (hd2 :: hd1 :: acc)
+        in
+    
+    match xs with
+    | [] -> []
+    | [elem] -> [elem]
+    | _ -> {
+        let split = splitList xsLen xs [] [] in
+        List.rev (mergeListsRev (mergesort pred (fst split)) (mergesort pred (snd split)) [])
+    }
+    ;;
+
+
+
+let rec mergesort pred xs =
+    let rec mergeListsRev l1 l2 acc =
+        match (l1, l2) with
+        | ([], []) -> acc
+        | ([], _) -> mergeListsRev l1 (List.tl l2) (List.hd l2 :: acc) (* If l1 is empty, add from l2*)
+        | (_, []) -> mergeListsRev (List.tl l1) l2 (List.hd l1 :: acc) (* If l2 is empty, add from l1*)
+        | (hd1::tl1, hd2::tl2) -> if pred hd1 hd2 then mergeListsRev tl1 tl2 (hd1 :: hd2 :: acc)
+                                    else mergeListsRev tl1 tl2 (hd2 :: hd1 :: acc)
+        in
+
+    match xs with
+    | [] -> []
+    | [elem] -> [elem]
+    | _ -> {
+        let split = ([], []) in
+        List.rev (mergeListsRev (mergesort pred (fst split)) (mergesort pred (snd split)) [])
+    }
+    ;;
