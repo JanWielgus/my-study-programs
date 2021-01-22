b0ec0d24ae119329ac9c97318895ba461633636f
Jan Wielgus <jan.wielgus12@gmail.com>
2021-01-22 13:48:36 +0100

    Paradygm cw 12

---
+++ Sem3/ParadygmatyProgramowania/Cwiczenia/ScalaSBTLaboratorium/src/main/scala/lista12.scala
@@ -0,0 +1,35 @@
+// Jan Wielgus
+
+import akka.actor._
+
+
+class Player (ID: Int) extends Actor
+{
+    override def receive = {
+        case Player.Ping(pingNum) =>
+            println("pong")
+            val nextPingNum = pingNum + 1;
+            if (nextPingNum < Player.MaxPings) {
+                println("ping")
+                sender() ! Player.Ping(nextPingNum)
+            }
+    }
+}
+
+object Player {
+    def props(ID: Int) = Props(classOf[Player], ID)
+    case class Ping(pingNum: Int)
+    val MaxPings = 10
+}
+
+
+object Main extends App {
+    val actorSystem = ActorSystem("MyActorSystem")
+    val pingerOne = actorSystem.actorOf(Player.props(1))
+    val pingerTwo = actorSystem.actorOf(Player.props(2))
+
+    pingerOne.tell(Player.Ping(0), pingerTwo)
+
+    Thread.sleep(1000)
+    actorSystem.terminate()
+}
